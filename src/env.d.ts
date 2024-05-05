81ca0f37da840abdb62b51869f49658936c27902
Karsten Zhou <54686209+XiaoSong-CPE@users.noreply.github.com>
2024-05-05 19:35:14 +0800

    tailwind bug fix

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,7 @@
 /// <reference types="vite/client" />
 
-declare var tailwind: {
-    config: any;
-}
+declare var tailwind: undefined | { config: any };
+declare var tailwindconfig: any;
 declare var mw: any;
 declare var CHP: Promise<any>;
 declare var dev: string | false;
--- src/ts/tailwindConfig.ts
+++ src/ts/tailwindConfig.ts
@@ -1,2 +1,7 @@
 // tailwindcss的配置文件
-window.tailwind.config = {};
+let config: any = {};
+typeof window.tailwind === "undefined"
+  ? // 如果window.tailwind不存在，就将config赋值给window.tailwindconfig作为预处理
+    (window.tailwindconfig = config)
+  : // 如果window.tailwind存在，就直接修改它
+    (window.tailwind.config = config);
