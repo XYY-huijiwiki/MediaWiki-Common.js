c551a40b99cd86546f1fb7d05c75e29c2da0afad
Karsten Zhou <54686209+XiaoSong-CPE@users.noreply.github.com>
2024-05-05 19:46:41 +0800

    remove tailwind config

--- src/ts/tailwindConfig.ts
+++
@@ -1,7 +0,0 @@
-// tailwindcss的配置文件
-let config: any = {};
-typeof window.tailwind === "undefined"
-  ? // 如果window.tailwind不存在，就将config赋值给window.tailwindconfig作为预处理
-    (window.tailwindconfig = config)
-  : // 如果window.tailwind存在，就直接修改它
-    (window.tailwind.config = config);
