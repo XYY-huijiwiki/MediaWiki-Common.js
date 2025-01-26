775519bc1f0210e2a96f188e377cb815e91d563a
Karsten Zhou <54686209+XiaoSong-CPE@users.noreply.github.com>
2025-01-26 10:00:43 +0800

    add: new dev mode

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,42 @@
-import "./ts/setIconList";
-import "./ts/setLanguage";
-import "./ts/tailwindConfig";
-import "./ts/githubFilesDisplay";
+import { debounce } from "lodash-es";
+
+(async () => {
+  // ===== dev mode =====
+  let dev = import.meta.env.DEV;
+  let devServer = new URL(location.href).searchParams.get("dev-server");
+  // if in prod mode and `?dev-server` is set, load dev server
+  if (!dev && devServer) {
+    await import(
+      /* @vite-ignore */
+      devServer
+    );
+    return;
+  }
+  // if in dev mode and hasn't been redirected yet, redirect to target url with `?dev-server`
+  if (dev && location.hostname !== "xyy.huijiwiki.com") {
+    let targetUrl = new URL("https://xyy.huijiwiki.com/wiki/首页");
+    targetUrl.searchParams.set("dev-server", import.meta.url);
+    location.href = targetUrl.href;
+    return;
+  }
+  // if in dev mode, add `?dev-server` to all links
+  const observer = new MutationObserver(
+    debounce(() => {
+      const devServerValue = "YOUR_DEV_SERVER";
+      document.querySelectorAll("a").forEach((a) => {
+        const url = new URL(a.href);
+        if (url.origin === location.origin) {
+          url.searchParams.set("dev-server", devServerValue);
+          a.href = url.href;
+        }
+      });
+    }, 200)
+  );
+  observer.observe(document.body, { childList: true, subtree: true });
+
+  // ===== prod mode =====
+  await import("./ts/setIconList.mts");
+  await import("./ts/setLanguage.mts");
+  await import("./ts/tailwindConfig.mts");
+  await import("./ts/githubFilesDisplay.mts");
+})();
